98918aa6d81f2ceefed6d251d06cbbf350157c7f
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-19 02:53:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function getToken() {
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
   const url = `https://api.github.com/repos/${repo}/forks`;
-  fetch(repo, {
+  fetch(url, {
     headers: {
       Authorization: `token ${getToken()}`
     }
